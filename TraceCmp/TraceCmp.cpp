f3aef147816859581725dad549d7fbb271a5580c
Blaž Satler <GTA.blaz.satler@gmail.com>
2020-06-19 09:20:45 +0200

    dodal temlejno funkcionalnost

--- TraceCmp/TraceCmp.cpp
+++ TraceCmp/TraceCmp.cpp
@@ -8,15 +8,16 @@
 
 using namespace std;
 
-int main()
+int main() //////////////Diff on line 2187//////////////////
 {
-    string oldpath, newpath, outpath;
-    cout << "Unpatched trace file path: ";
+    string oldpath = "..\\logold.txt", newpath = "..\\lognew.txt", outpath = "..\\out.txt";
+    /*cout << "Unpatched trace file path: ";
     cin >> oldpath;
     cout << "\nPatched trace file path: ";
     cin >> newpath;
     cout << "\nOutput file path: ";
-    cin >> outpath;
+    cin >> outpath;*/
+
     ofstream outfile;
     ifstream oldfile;
     ifstream newfile;
@@ -33,6 +34,7 @@ int main()
     int diffcount = 1;
 
     regex a("^[A-Za-z0-9\\_\\-]+\\+0x");
+    regex b("0x+[0-9a-fA-F]+");
 
     getline(oldfile, line1);
     getline(newfile, line2);
@@ -45,13 +47,14 @@ int main()
     int consecutivecountfail = 0;
     int consecutivecountsuccess = 0;
 
+
     while (getline(oldfile, line1) && getline(newfile, line2)) {
         temp1 = regex_replace(line1, a, "");
         temp2 = regex_replace(line2, a, "");
         currold = stoi(temp1, 0, 16);
         currnew = stoi(temp2, 0, 16);
 
-        if (currold - prevold > 15 || currnew - prevnew > 15);//check for 15byte offset. Ignore and compare next instrction
+        if (currold - prevold > 15 || currnew - prevnew > 15 || currold - prevold < 1 || currnew - prevnew < 1);//check for 15byte offset. Ignore and compare next instrction
         else if (((currold - prevold) != (currnew - prevnew)) && !flag) {
             cout << "\n Difference found on line " << linecount << "\n";
             outfile << "\n/////" << diffcount << ". Difference HERE/////\n";
